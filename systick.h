6cd28e918a42bcd69ec58e449b24b56447bfed0e
Rasmus Haugaard <rasmus.l.haugaard@gmail.com>
2017-04-11 04:24:16 +0200

    fix 80MHz

--- systick.h
+++ systick.h
@@ -1,8 +1,8 @@
 #ifndef _EMP_H
 #define _EMP_H
 
-#define CLK 16000000
-#define SYSTICK_RELOAD_VALUE 1600
+#define CLK 80000000
+#define SYSTICK_RELOAD_VALUE 8000
 #define SYS_FREQ (CLK / SYSTICK_RELOAD_VALUE)
 
 extern volatile INT16U ticks;
