d8e8d5e907091cd470b91eb02ff6a8791932c60f
Rasmus Haugaard <rasmus.l.haugaard@gmail.com>
2017-04-20 10:11:42 +0200

    atomic ticks decrementation. only global millis and global ticks

--- rtcs.h
+++ rtcs.h
@@ -1,14 +1,18 @@
 #ifndef _RTCS_H
 #define _RTCS_H
 
+#include <stdint.h>
+#include "emp_type.h"
+
 typedef unsigned char TASK;
 typedef unsigned char SEM;
 typedef unsigned char QUEUE;
 
 #define MAX_TASKS 16
-#define MAX_TASK_NAME_LENGTH 10
 #define MAX_QUEUES 16
 #define MAX_SEMAPHORES 64 // The first 2xMAX_QUEUES are reserved for the queues.
+
+#define TASK_NAME_LENGTH 10
 
 #define NO_TASK 0
 #define TASK_READY 1
@@ -25,20 +29,13 @@ typedef unsigned char QUEUE;
 
 #define ERROR_TASK 0xFF // Task error
 
-INT16U now_micros();
+INT16U now_ticks();
 INT16U now_millis();
 
 INT16U millis(INT16U);
 INT16U micros(INT16U);
 
 void set_state(INT8U);
-
-INT8U get_task_condition(TASK id);
-INT8U get_task_state(TASK id);
-SEM get_task_sem(TASK id);
-INT16U get_task_timer(TASK id);
-const INT8U* get_task_name(TASK id);
-
 void wait(INT16U);
 BOOLEAN wait_sem(INT8U, INT16U);
 void signal(INT8U);
@@ -47,8 +44,14 @@ QUEUE create_queue();
 BOOLEAN put_queue(QUEUE, INT8U, INT16U);
 BOOLEAN get_queue(QUEUE, INT8U*, INT16U);
 
-TASK create_task(void (*tf)(INT8U, INT8U, INT8U, INT8U), INT8U* name);
+TASK create_task(void (*tf)(INT8U, INT8U, INT8U, INT8U), char* name);
 SEM create_sem();
+
+INT8U get_task_condition(TASK id);
+const INT8U* get_task_name(TASK id);
+INT8U get_task_state(TASK id);
+SEM get_task_sem(TASK id);
+INT16U get_task_timer(TASK id);
 
 void init_rtcs();
 void schedule();
